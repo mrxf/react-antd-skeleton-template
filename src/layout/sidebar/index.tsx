eca0eb89b5169194f91221ae55ebb47decd90113
ZHANGSHUYUAN <zhangshuyuan@kingsoft.com>
2020-12-23 16:46:25 +0800

    bugfix: 侧边栏点击导航后没有更新面包屑

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -6,6 +6,7 @@ import MenuSkeleton from "./components/menuSkeleton";
 import { IRouteItem } from "src/constants/interfaces/IRouterItem";
 import { NavLink, RouteComponentProps, withRouter } from "react-router-dom";
 import { concatPath } from "src/utils/flatRoutes";
+import { GlobalState } from "src/components/globalState";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -14,28 +15,34 @@ interface SiderBarProps extends RouteComponentProps {
   routeItems: IRouteItem[];
 }
 
-const getItem = (item: IRouteItem, prefix = "/") => {
-  // 两种隐藏二级侧边栏的情况
-  if (item.hide) {
-    return undefined;
-  }
-  const path = concatPath(item.value, prefix);
-  return (
-    <Menu.Item key={path}>
-      <NavLink to={path}>
-        {item.icon}
-        <span className="nav-text">{item.name}</span>
-      </NavLink>
-    </Menu.Item>
-  );
-};
-
 const SiderBar: React.FC<SiderBarProps> = ({ routeItems, history }) => {
   const { isValidating: isLogin } = useSWR("/antd/userinfo");
   const [collapsed, setCollapsed] = useState<boolean>(false);
+  const [, dispatch] = GlobalState.useContainer();
   const { pathname } = history.location;
   const openKey = "/" + pathname.split("/")[1]; // 数字是根据路由前面有多少个默认的key处理的，如果前面有更多，则+1
 
+  const getItem = (item: IRouteItem, prefix = "/") => {
+    // 隐藏二级侧边栏的情况
+    if (item.hide) {
+      return undefined;
+    }
+    const path = concatPath(item.value, prefix);
+    return (
+      <Menu.Item key={path}>
+        <NavLink
+          onClick={() =>
+            // 每次切换侧边栏地址，都全局更新面包屑信息
+            dispatch({ type: "update_route_by_path", pathname: path })
+          }
+          to={path}
+        >
+          {item.icon}
+          <span className="nav-text">{item.name}</span>
+        </NavLink>
+      </Menu.Item>
+    );
+  };
   return (
     <Sider
       className={`${style.side} full-skeleton`}
