d7434f5195a95a18ac8452d14c723f9917b9ae48
ZHANGSHUYUAN <zhangshuyuan@kingsoft.com>
2020-12-30 20:43:27 +0800

    lint fixed

--- src/utils/getMenuItem/index.tsx
+++ src/utils/getMenuItem/index.tsx
@@ -1,8 +1,7 @@
 import { Menu } from "antd";
-import React, { Dispatch } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { IRouteItem } from "src/constants/interfaces/IRouterItem";
-import { IGlobalStateAction } from "src/hooks/useGlobalState";
 
 export const getMenuItem = (item: IRouteItem) => {
   // 隐藏二级侧边栏的情况
